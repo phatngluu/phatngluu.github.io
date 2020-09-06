1758aaa614486fca85305d42c7ed327a878ddb19
Phat Nguyen Luu <44693107+phatngluu@users.noreply.github.com>
2020-09-06 22:09:59 +0700

    Update auto-publish.sh

--- auto-publish.sh
+++ auto-publish.sh
@@ -6,4 +6,10 @@ rm -rf docs
 echo "Generating..."
 hugo
 
+echo "Commiting docs..."
+cd public && git add --all && git commit -m "auto-generate docs"
+
+echo "Pushing..."
+git push
+
 echo "Published to /docs."
