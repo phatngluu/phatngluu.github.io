13127f22146f37f65693c56a552851d9eedc16c5
Phat Nguyen Luu <44693107+phatngluu@users.noreply.github.com>
2020-09-06 22:06:12 +0700

    Update auto-publish.sh

--- auto-publish.sh
+++ auto-publish.sh
@@ -1,33 +1,9 @@
 #!/bin/sh
 
-if [ "`git status -s`" ]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
-
 echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
+rm -rf docs
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public gh-pages
-
-echo "Removing existing files"
-rm -rf public/*
-
-echo "Generating site"
+echo "Generating..."
 hugo
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
-
-echo "Pushing to github"
-git push --all
-
-echo "Deleting public folder"
-rm -rf public
-
-echo "Published."
+echo "Published to /docs."
