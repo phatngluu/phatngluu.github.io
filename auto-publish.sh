aa80cac6cde01414e3ce166b6e7ddcc5ac4d7141
Phat Nguyen Luu <44693107+phatngluu@users.noreply.github.com>
2020-09-11 10:13:00 +0700

    Update auto-publish.sh

--- auto-publish.sh
+++ auto-publish.sh
@@ -7,7 +7,7 @@ echo "Generating..."
 hugo
 
 echo "Moving uploads to static/uploads"
-mv -f docs/uploads docs/static/uploads
+mv -f /docs/uploads /docs/static/uploads
 
 echo "Commiting docs..."
 cd docs && git add --all && git commit -m "auto-generate docs"
